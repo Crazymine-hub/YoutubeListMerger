8d0c43d0cfb6d9a6f0d8dcd326228050221284d2
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-06-15 23:32:55 +0200

    Started UI Design

--- YoutubeListMerger/MergerForm.cs
+++ YoutubeListMerger/MergerForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace YoutubeListMerger
 {
-    public partial class Form1 : Form
+    public partial class MergerForm : Form
     {
-        public Form1()
+        public MergerForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ YoutubeListMerger/PlaylistAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeListMerger
+{
+    public partial class PlaylistAnalyzer : UserControl
+    {
+        public PlaylistAnalyzer(Uri targetUrl)
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ListVideos(Uri targetUri)
+        {
+
+        }
+    }
+}
