0d62ff11d831227fd6e027091d0fa87473ea458b
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-06-19 18:15:51 +0200

    Added Playlist and Video Listing

--- YoutubeListMerger/MergerForm.Designer.cs
+++ YoutubeListMerger/MergerForm.Designer.cs
@@ -29,45 +29,147 @@ namespace YoutubeListMerger
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.Workspace = new System.Windows.Forms.SplitContainer();
+            this.PlaylistList = new System.Windows.Forms.ListBox();
+            this.playlistAnalyzerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.PlaylistTitleLabel = new System.Windows.Forms.Label();
+            this.VideoList = new System.Windows.Forms.ListBox();
+            this.VideoListLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.videoInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Workspace)).BeginInit();
+            this.Workspace.Panel1.SuspendLayout();
+            this.Workspace.Panel2.SuspendLayout();
+            this.Workspace.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playlistAnalyzerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoInfoBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // Workspace
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
+            this.Workspace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Workspace.IsSplitterFixed = true;
+            this.Workspace.Location = new System.Drawing.Point(0, 23);
+            this.Workspace.Name = "Workspace";
             // 
-            // splitContainer1.Panel1
+            // Workspace.Panel1
             // 
-            this.splitContainer1.Panel1.AutoScroll = true;
-            this.splitContainer1.Size = new System.Drawing.Size(584, 461);
-            this.splitContainer1.SplitterDistance = 272;
-            this.splitContainer1.SplitterWidth = 50;
-            this.splitContainer1.TabIndex = 0;
+            this.Workspace.Panel1.AutoScroll = true;
+            this.Workspace.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Workspace.Panel1.Controls.Add(this.PlaylistList);
+            this.Workspace.Panel1.Controls.Add(this.PlaylistTitleLabel);
+            // 
+            // Workspace.Panel2
+            // 
+            this.Workspace.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Workspace.Panel2.Controls.Add(this.VideoList);
+            this.Workspace.Panel2.Controls.Add(this.VideoListLabel);
+            this.Workspace.Size = new System.Drawing.Size(584, 438);
+            this.Workspace.SplitterDistance = 272;
+            this.Workspace.SplitterWidth = 50;
+            this.Workspace.TabIndex = 0;
+            // 
+            // PlaylistList
+            // 
+            this.PlaylistList.DataSource = this.playlistAnalyzerBindingSource;
+            this.PlaylistList.DisplayMember = "Title";
+            this.PlaylistList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PlaylistList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.PlaylistList.HorizontalScrollbar = true;
+            this.PlaylistList.ItemHeight = 18;
+            this.PlaylistList.Location = new System.Drawing.Point(0, 25);
+            this.PlaylistList.Name = "PlaylistList";
+            this.PlaylistList.Size = new System.Drawing.Size(272, 413);
+            this.PlaylistList.TabIndex = 4;
+            this.PlaylistList.ValueMember = "ID";
+            this.PlaylistList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.PlaylistList_DrawItem);
+            this.PlaylistList.SelectedIndexChanged += new System.EventHandler(this.PlaylistList_SelectedIndexChanged);
+            // 
+            // playlistAnalyzerBindingSource
+            // 
+            this.playlistAnalyzerBindingSource.DataSource = typeof(YoutubeListMerger.Classes.PlaylistAnalyzer);
+            // 
+            // PlaylistTitleLabel
+            // 
+            this.PlaylistTitleLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.PlaylistTitleLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PlaylistTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlaylistTitleLabel.Location = new System.Drawing.Point(0, 0);
+            this.PlaylistTitleLabel.Name = "PlaylistTitleLabel";
+            this.PlaylistTitleLabel.Size = new System.Drawing.Size(272, 25);
+            this.PlaylistTitleLabel.TabIndex = 0;
+            this.PlaylistTitleLabel.Text = "Playlists";
+            // 
+            // VideoList
+            // 
+            this.VideoList.DataSource = this.videoInfoBindingSource;
+            this.VideoList.DisplayMember = "Title";
+            this.VideoList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VideoList.HorizontalScrollbar = true;
+            this.VideoList.Location = new System.Drawing.Point(0, 25);
+            this.VideoList.Name = "VideoList";
+            this.VideoList.Size = new System.Drawing.Size(262, 413);
+            this.VideoList.TabIndex = 3;
+            this.VideoList.ValueMember = "ID";
+            // 
+            // VideoListLabel
+            // 
+            this.VideoListLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.VideoListLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.VideoListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VideoListLabel.Location = new System.Drawing.Point(0, 0);
+            this.VideoListLabel.Name = "VideoListLabel";
+            this.VideoListLabel.Size = new System.Drawing.Size(262, 25);
+            this.VideoListLabel.TabIndex = 2;
+            this.VideoListLabel.Text = "Videos";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(584, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // videoInfoBindingSource
+            // 
+            this.videoInfoBindingSource.DataSource = typeof(YoutubeListMerger.Classes.VideoInfo);
             // 
             // MergerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 461);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.Workspace);
+            this.Controls.Add(this.button1);
             this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Name = "MergerForm";
             this.Text = "PlaylistMerger";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.Workspace.Panel1.ResumeLayout(false);
+            this.Workspace.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Workspace)).EndInit();
+            this.Workspace.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.playlistAnalyzerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoInfoBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer Workspace;
+        private System.Windows.Forms.Label PlaylistTitleLabel;
+        private System.Windows.Forms.ListBox PlaylistList;
+        private System.Windows.Forms.ListBox VideoList;
+        private System.Windows.Forms.Label VideoListLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource playlistAnalyzerBindingSource;
+        private System.Windows.Forms.BindingSource videoInfoBindingSource;
     }
 }
 
--- YoutubeListMerger/MergerForm.cs
+++ YoutubeListMerger/MergerForm.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeListMerger.Classes;
 
 namespace YoutubeListMerger
 {
     public partial class MergerForm : Form
     {
+        private YouTubeService youTube = new YouTubeService(new BaseClientService.Initializer() { ApiKey = File.ReadAllText("./API.key") });
+        public List<PlaylistAnalyzer> Playlists { get; private set; }
+        public Random random = new Random();
+
         public MergerForm()
         {
             InitializeComponent();
@@ -20,6 +28,64 @@ namespace YoutubeListMerger
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void PlaylistList_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index == -1) return;
+            ListBox list = (ListBox)sender;
+            PlaylistAnalyzer playlist = (PlaylistAnalyzer)list.Items[e.Index];
+
+            // Draw the background of the ListBox control for each item.
+            e.DrawBackground();
+            // Define the default color of the brush as black.
+            Brush textBrush = Brushes.Black;
+            Brush textBrushHighlight = Brushes.White;
+
+
+            RectangleF bar = new RectangleF(e.Bounds.X + 2, e.Bounds.Y + 1, e.Bounds.Width * playlist.Progress - 2, e.Bounds.Height - 2);
+            RectangleF unusedBar = new RectangleF(e.Bounds.X + bar.Width, e.Bounds.Y + 1, e.Bounds.Width - bar.Width, e.Bounds.Height - 2);
+            if (playlist.Progress < 1)
+                e.Graphics.FillRectangle(Brushes.Green, bar);
+            if (!e.State.HasFlag(DrawItemState.Selected) && playlist.Progress < 1)
+                e.Graphics.FillRectangle(Brushes.LightGray, unusedBar);
+
+            // Draw the current item text based on the current Font 
+            // and the custom brush settings.
+            var alignment = new StringFormat(StringFormat.GenericDefault) { LineAlignment = StringAlignment.Center };
+            e.Graphics.IntersectClip(bar);
+            e.Graphics.DrawString(playlist.ToString(), e.Font, textBrushHighlight, e.Bounds, alignment);
+
+            e.Graphics.ResetClip();
+            e.Graphics.ExcludeClip(new Region(bar));
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            if (e.State.HasFlag(DrawItemState.Selected))
+                e.Graphics.FillRectangle(Brushes.LightBlue, e.Bounds);
+            e.Graphics.DrawString(playlist.ToString(), e.Font, textBrush, e.Bounds, alignment);
+
+
+            if (e.State.HasFlag(DrawItemState.Focus))
+            {
+                Rectangle focusRect = new Rectangle(e.Bounds.X, e.Bounds.Y + 1, e.Bounds.Width, e.Bounds.Height - 2);
+                e.Graphics.DrawRectangle(new Pen(Brushes.Black, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash }, focusRect);
+            }
+        }
+
+        private void PlaylistList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action<object, EventArgs>)PlaylistList_SelectedIndexChanged, sender, e);
+                return;
+            }
+            videoInfoBindingSource.Clear();
+            var playlist = (PlaylistAnalyzer)PlaylistList.SelectedItem;
+            if (playlist == null || !playlist.IsCustom && !playlist.WorkerTask.IsCompleted)
+                return;
+
+
+            foreach (var vid in playlist.VideoList)
+                videoInfoBindingSource.Add(vid);
         }
     }
 }
