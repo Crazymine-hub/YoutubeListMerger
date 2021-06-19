829fcb99f0fe3e0df191a71edf6250b5e456bf36
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-06-19 21:30:34 +0200

    Added Analyzer for Channel Urls

---
+++ YoutubeListMerger/ChannelAnalyzer.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace YoutubeListMerger
+{
+    partial class ChannelAnalyzer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.channelUrlLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 79);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(447, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.label1.Size = new System.Drawing.Size(471, 49);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Processing Channel URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // channelUrlLabel
+            // 
+            this.channelUrlLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.channelUrlLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.channelUrlLabel.Location = new System.Drawing.Point(0, 49);
+            this.channelUrlLabel.Name = "channelUrlLabel";
+            this.channelUrlLabel.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.channelUrlLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.channelUrlLabel.Size = new System.Drawing.Size(471, 27);
+            this.channelUrlLabel.TabIndex = 2;
+            this.channelUrlLabel.Text = "label2";
+            this.channelUrlLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.channelUrlLabel.UseWaitCursor = true;
+            // 
+            // ChannelAnalyzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 112);
+            this.ControlBox = false;
+            this.Controls.Add(this.channelUrlLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ChannelAnalyzer";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Channel URL Analyzer";
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label channelUrlLabel;
+    }
+}
---
+++ YoutubeListMerger/ChannelAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+
+namespace YoutubeListMerger
+{
+    public partial class ChannelAnalyzer : Form
+    {
+        public string ChannelId { get; internal set; }
+        private Uri channelUri;
+        public ChannelAnalyzer(Uri ytChannelUri)
+        {
+            InitializeComponent();
+            Load += ChannelAnalyzer_Load;
+            channelUri = ytChannelUri;
+            channelUrlLabel.Text = channelUri.ToString();
+        }
+
+        private void ChannelAnalyzer_Load(object sender, EventArgs e)
+        {
+            Task.Run(Analyze);
+        }
+
+        private void Analyze()
+        {
+            WebRequest channelPage = WebRequest.Create(channelUri);
+            var response = channelPage.GetResponse();
+            var contentReader = new StreamReader(response.GetResponseStream());
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(contentReader.ReadToEnd());
+            Uri channelCleanUri = new Uri(doc.DocumentNode.SelectNodes("/html/body/span[@itemprop='author']/link[@itemprop='url']")[0].Attributes["href"].Value);
+            var path = channelCleanUri.LocalPath.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+            if (path.Length == 2 && path[0] == "channel")
+                ChannelId = path[1];
+            else
+                throw new Exception("No valid Channel URL found.");
+            Invoke((MethodInvoker)Close);
+        }
+    }
+}
