0991f99133385f989367bca0c9a2a665342d2949
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-07-13 20:50:57 +0200

    Added Form to Change Playlist order before merge.

---
+++ YoutubeListMerger/PlaylistOrder.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace YoutubeListMerger
+{
+    partial class PlaylistOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.moveDownButton = new System.Windows.Forms.Button();
+            this.moveUpButton = new System.Windows.Forms.Button();
+            this.orderList = new System.Windows.Forms.ListBox();
+            this.playlistSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playlistSource)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select the order in wich the playlists will be processed.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.moveDownButton);
+            this.panel1.Controls.Add(this.moveUpButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(392, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(50, 269);
+            this.panel1.TabIndex = 1;
+            // 
+            // moveDownButton
+            // 
+            this.moveDownButton.AutoSize = true;
+            this.moveDownButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.moveDownButton.Enabled = false;
+            this.moveDownButton.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveDownButton.Location = new System.Drawing.Point(5, 31);
+            this.moveDownButton.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
+            this.moveDownButton.Name = "moveDownButton";
+            this.moveDownButton.Size = new System.Drawing.Size(40, 26);
+            this.moveDownButton.TabIndex = 1;
+            this.moveDownButton.Text = "";
+            this.moveDownButton.UseVisualStyleBackColor = true;
+            this.moveDownButton.Click += new System.EventHandler(this.moveDownButton_Click);
+            // 
+            // moveUpButton
+            // 
+            this.moveUpButton.AutoSize = true;
+            this.moveUpButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.moveUpButton.Enabled = false;
+            this.moveUpButton.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveUpButton.Location = new System.Drawing.Point(5, 5);
+            this.moveUpButton.Name = "moveUpButton";
+            this.moveUpButton.Size = new System.Drawing.Size(40, 26);
+            this.moveUpButton.TabIndex = 0;
+            this.moveUpButton.Text = "";
+            this.moveUpButton.UseVisualStyleBackColor = true;
+            this.moveUpButton.Click += new System.EventHandler(this.moveUpButton_Click);
+            // 
+            // orderList
+            // 
+            this.orderList.DataSource = this.playlistSource;
+            this.orderList.DisplayMember = "Title";
+            this.orderList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.orderList.FormattingEnabled = true;
+            this.orderList.Location = new System.Drawing.Point(0, 13);
+            this.orderList.Name = "orderList";
+            this.orderList.Size = new System.Drawing.Size(392, 269);
+            this.orderList.TabIndex = 2;
+            this.orderList.ValueMember = "UniqueId";
+            this.orderList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // playlistSource
+            // 
+            this.playlistSource.DataSource = typeof(YoutubeListMerger.Classes.PlaylistAnalyzer);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cancelButton);
+            this.panel2.Controls.Add(this.okButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 282);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(442, 36);
+            this.panel2.TabIndex = 3;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(280, 5);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 26);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.okButton.Location = new System.Drawing.Point(362, 5);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 26);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // PlaylistOrder
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(442, 318);
+            this.Controls.Add(this.orderList);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(290, 357);
+            this.Name = "PlaylistOrder";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "PlaylistOrder";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playlistSource)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button moveUpButton;
+        private System.Windows.Forms.ListBox orderList;
+        private System.Windows.Forms.Button moveDownButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        internal System.Windows.Forms.BindingSource playlistSource;
+    }
+}
---
+++ YoutubeListMerger/PlaylistOrder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeListMerger.Classes;
+
+namespace YoutubeListMerger
+{
+    public partial class PlaylistOrder : Form
+    {
+        public PlaylistOrder(IEnumerable<PlaylistAnalyzer> playlists)
+        {
+            foreach (var playlist in playlists)
+                playlistSource.Add(playlist);
+            InitializeComponent();
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            moveUpButton.Enabled = orderList.SelectedIndex >= 0;
+            moveDownButton.Enabled = orderList.SelectedIndex < playlistSource.Count;
+        }
+
+        private void moveUpButton_Click(object sender, EventArgs e)
+        {
+            int index = orderList.SelectedIndex;
+            object item = playlistSource[index];
+            playlistSource.RemoveAt(index);
+            playlistSource.Insert(index - 1, item);
+        }
+
+        private void moveDownButton_Click(object sender, EventArgs e)
+        {
+            int index = orderList.SelectedIndex;
+            object item = playlistSource[index];
+            playlistSource.RemoveAt(index);
+            playlistSource.Insert(index + 1, item);
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
